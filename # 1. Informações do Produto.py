02843f70639762b787a2f7697da4e91d64fd216c
Rafaelbrchalegrebr <rafaelbrchalegrebr@gmail.com>
2025-08-14 23:49:34 -0300

    Fiz para mostrar a informação do produto

---
+++ # 1. Informações do Produto.py
@@ -0,0 +1,9 @@
+# 1. Declaração das variáveis
+nome_produto = "Tesoura"
+preco_produto = 2.50
+quantidade_estoque = 100
+
+# 2. Exibição das informações do produto
+print("Produto:", nome_produto)
+print(f"Preço: R$ {preco_produto:.2f}")
+print(f"Quantidade em estoque: {quantidade_estoque} unidades")
